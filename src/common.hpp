49e60dba946b3822d141cca31933ba9bf316cee4
FoolishMe <2324532557@qq.com>
2020-12-30 17:27:17 +0800

    .

--- src/common.hpp
+++ src/common.hpp
@@ -6,5 +6,20 @@
 typedef std::vector<uint16_t> bytevec;
 typedef unsigned int uint;
 
+bytevec readfile(ifstream& in) {
+    bytevec bdata;
+    while (!in.eof()) {
+        char byte; in.get(byte);
+        if (in.eof()) break;
+        bdata.push_back(byte);
+    }
+    return bdata;
+}
+
+void writefile(ofstream& out, const bytevec& vec) {
+    for (auto c: vec) {
+        out.put(c);
+    }
+}
 
 #endif
