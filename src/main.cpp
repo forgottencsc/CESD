87c1403fb5b3e901582d279d54dd7944a7dd4c9f
FoolishMe <2324532557@qq.com>
2021-01-04 13:54:03 +0800

    /.

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,51 @@
 #include <bits/stdc++.h>
 #include "gao.hpp"
-
+#include "verify.hpp"
+#include "common.hpp"
 using namespace std;
-
-using namespace std::string_literals;
-
-using std::experimental::filesystem::path;
+using namespace detail;
 
 int main(void) {
     uint64_t key;
-    wstring type, ipath, opath;
-    wcin >> type;
 
-    if (type == L"compress") {
-        wcin >> ipath >> key >> opath;
+    string type, ipath, opath;
+    cin >> type;
+
+    if (type == "compress") {
+        cin >> ipath >> key >> opath;
         huffman_compessor c;
         xorplus_cryptor x(key);
-        ofstream ofs(path(opath).string(), std::ios::binary);
+        ofstream ofs(opath, std::ios::binary);
         detail::writefile(ofs, gao::gao(ipath, c, x));
         ofs.close();
     }
-    else if (type == L"decompress") {
-        wcin >> ipath >> key >> opath;
+    else if (type == "decompress") {
+        cin >> ipath >> key >> opath;
         huffman_compessor c;
         xorplus_cryptor x(key);
-        ifstream ifs(path(ipath).string(), std::ios::binary);
+        ifstream ifs(ipath, std::ios::binary);
         gao::ungao(opath, detail::readfile(ifs), c, x);
         ifs.close();
+    }
+    else if (type == "verify") {
+        cin >> ipath >> key >> opath;
+        huffman_compessor c;
+        xorplus_cryptor x(key);
+        ifstream ifs(ipath, std::ios::binary);
+        bytevec decrypted_data = x.decrypt(readfile(ifs));
+        ifs.close();
+        bytevec decompressed_data = c.decompress(decrypted_data);
+
+        string result = verify::verify(decompressed_data, opath);
+        if (result == "") {
+            cout << "succ" << endl;
+        }
+        else {
+            cout << result << endl;
+        }
     }
     else {
         cout << "Usage: <type> <ipath> <key> <opath>" << endl;
     }
-        
     return 0;
 }
---
+++ src/verify.hpp
@@ -0,0 +1,164 @@
+#ifndef VERIFY_HPP
+#define VERIFY_HPP
+#include "common.hpp"
+#include "pack.hpp"
+
+
+namespace verify{
+using namespace std;
+using namespace detail;
+using namespace pack;
+
+typedef pair<string, char> file;
+map<file, bytevec>unpack_to_map(const bytevec& data) {
+    map<file, bytevec>files;
+
+    uint ptr = 0;
+
+    auto get_filename = [&]() {
+        uint namelen = btoi32(subvec(data, ptr, ptr + NUMLEN));
+        ptr += NUMLEN;
+        bytevec namevec = subvec(data, ptr, ptr + namelen);
+        ptr += namelen;
+        string filename = ""; for (auto c: namevec) filename += c;
+        return filename;
+    };
+    
+    auto get_content = [&]() {
+        uint content_len = btoi32(subvec(data, ptr, ptr + NUMLEN));
+        ptr += NUMLEN;
+        bytevec content = subvec(data, ptr, ptr + content_len);
+        ptr += content_len;
+        return content;
+    };
+
+    auto get_sonnum = [&]() {
+        uint res = btoi32(subvec(data, ptr, ptr + NUMLEN));
+        ptr += NUMLEN;
+        return res;
+    };
+
+    struct info{
+        string name;
+        uint number_of_sons;
+    };
+    
+    queue<info>q;
+    q.push({"", 1}); 
+    while (q.size()) {
+        auto item = q.front(); q.pop();
+        string thisdir = item.name;
+        uint sz = item.number_of_sons;
+        while (sz--) {
+            string filename = get_filename();
+            string filepath = (thisdir == "") ? filename : thisdir + "\\" + filename;
+            char dir_tag = data[ptr++];
+            if (dir_tag == '1') {
+                cerr << "Get dir " << filepath << endl;
+                files[{filepath, dir_tag}] = {};
+            }
+            else {
+                cerr << "Get file " << filepath << endl;
+                bytevec content = get_content();
+                files[{filepath, dir_tag}] = content;
+            }
+            uint number_of_sons = get_sonnum();
+            q.push({filepath, number_of_sons});
+        }
+    }
+    return files;
+}
+
+void outlog(string &s, string log) {
+    s += log + '\n';
+    // cerr << log << endl;
+}
+
+string verify(const bytevec& data, const string& path_str) {
+    string logs = "";
+    auto files = unpack_to_map(data);
+
+    struct info{
+        string path;
+        string filename;
+    };
+
+    // get the stem of the path
+    uint lb = 0, rb = path_str.size();
+    if (path_str.back() == '\\') {
+        string tmp_s = path_str;
+        tmp_s.pop_back();
+        rb--;
+        lb = tmp_s.find_last_of('\\') + 1;
+    }
+    else {
+        lb = path_str.find_last_of('\\') + 1;
+    }
+    string name = path_str.substr(lb, rb - lb);
+    
+    map<file, bool>thisfiles;
+
+    queue<info>q;
+    q.push({name, name});
+    while (q.size()) {
+        info item = q.front(); q.pop();
+        cerr << item.filename << endl;
+        uint number_of_sons = 0;
+        struct stat s_buf;
+        stat(item.path.c_str(), &s_buf);
+        if (!S_ISDIR(s_buf.st_mode)) { // not a directory
+            thisfiles[{item.path, '0'}] = 1;
+
+            ifstream fin(item.path, ios::binary);
+            bytevec data = readfile(fin); // get data
+            fin.close();
+            if (!files.count({item.path, '0'})) {
+                outlog(logs, "files " + item.path + " is not in compressed data.");
+            }
+            else {
+                if (files[{item.path, '0'}] != data) {
+                    outlog(logs, "files " + item.path + " is inconsistent in content.");
+                }
+            }
+        }
+        else {
+            DIR *dir = NULL;
+            struct dirent *entry;
+            if ((dir = opendir(item.path.c_str())) == NULL) {
+                cerr << "Directory Open failed." << endl;
+            }
+            else {
+                thisfiles[{item.path, '1'}] = 1;
+                if (!files.count({item.path, '1'})) {
+                    outlog(logs, "directory " + item.path + " is not in compressed data.");
+                }
+                else {
+                    // do nothing
+                }
+                while (entry = readdir(dir)) {
+                    string filename_str = entry->d_name;
+                    if (filename_str != "." && filename_str != "..") {
+                        q.push({item.path + "\\" + filename_str, filename_str});
+                        number_of_sons++;
+                    }
+                }
+            }
+        }
+    }
+
+    for (auto item: files) {
+        if (!thisfiles.count(item.first)) {
+            if (item.first.second == '0') {
+                outlog(logs, "files " + item.first.first + " is not in " + path_str + ".");
+            }
+            else {
+                outlog(logs, "directory " + item.first.first + " is not in " + path_str + ".");
+            }
+        }
+    }
+    return logs;
+}
+
+};
+
+#endif
